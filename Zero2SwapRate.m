49140b85beb4de64f20fc8aa017e6b270a2ad76d
guyute <benjamin.whitcher@gmail.com>
2014-12-11 18:14:11 -0500

    Updated Zero2SwapRate & DF_Vasicek2F to accept a vector of maturities

--- Zero2SwapRate.m
+++ Zero2SwapRate.m
@@ -1,7 +1,2 @@
-function c=Zer2SwapRate(T,DF)
-    SumDF=DF(1/2);
-    for i =2:T*2
-        SumDF=SumDF + DF(i/2);
-    end
-    c=(1-DF(T))./SumDF*2*100;
-end
+function c = ZeroToSwapRate(tau, DF)
+c = 200*(1-DF(tau))./sum(DF(0.5:0.5:tau),2);
